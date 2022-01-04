ef70aa07355422cfd9c6c858e528b7bce35e89d5
Click2Cloud-Prajwal <95343388+click2cloud-prajwal@users.noreply.github.com>
2022-01-04 18:15:09 +0530

    Update hosts.sh

--- Fornax/fornaxsrp/hosts.sh
+++ Fornax/fornaxsrp/hosts.sh
@@ -3,9 +3,9 @@
 set -x
 
 hostnamectl set-hostname arktos-test
-echo '192.168.1.210 arktos-test' | sudo tee -a /etc/hosts
+echo '$i arktos-test' | sudo tee -a /etc/hosts
 wget https://raw.githubusercontent.com/CentaurusInfra/mizar/dev-next/kernelupdate.sh
-sudo bash kernelupdate.sh	-y
+yes y | sudo bash kernelupdate.sh	-y
 init 6
 
 Done
